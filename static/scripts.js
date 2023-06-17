541d59025565b0e26d7beb458d88e74891ebde23
rohan19a <arohan19a2@gmail.com>
2023-06-17 11:00:50 -0700

    login, buttons

--- static/scripts.js
+++ static/scripts.js
@@ -12,7 +12,30 @@ function handleAdd() {
         newElementDiv.appendChild(input);
     }
     elementsDiv.appendChild(newElementDiv);
-  }
+}
+
+function handleLogin() {
+    login = document.getElementById("username");
+    password = document.getElementById("password");
+    if (verifyLogin(login.value, password.value)) {
+        addElements(login.value);
+        login.style.display = "none";
+        password.style.display = "none";
+}
+}
+
+function verifyLogin(login, password) {
+    if (login == "admin" && password == "admin") {
+        return true;
+    }
+    return false;
+}
+
+function addElements(username) {
+    //make an api request to get the elements
+    return true
+}
+
 
 function handleRemove() {
     var elementsDiv = document.getElementById("elements");
@@ -31,7 +54,7 @@ function handleSave() {
     console.log(data);
     $.ajax({
         type: "POST",
-        url: "/add",
+        url: "/create",
         data: JSON.stringify(data),
         contentType: "application/json; charset=utf-8",
         dataType: "json",
